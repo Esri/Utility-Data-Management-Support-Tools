30b19d0d86d939dad0eada5e0218c24a4f6919e9
Paul LeBlanc <pleblanc@esri.com>
2024-06-01 13:50:43 -0500

    add toolset to readme

--- build_toc.py
+++ build_toc.py
@@ -11,7 +11,8 @@ for folder in docs.glob("*"):
     print(folder.name)
 
     with (folder / "readme.md").open("w", encoding="utf-8") as writer:
-        writer.write("| Name | Alias | Description |\n| --- | --- | --- |\n")
+        header = ["Toolset", "Name", "Alias", "Description"]
+        writer.write("| {} |\n| {} |\n".format(" | ".join(header), " | ".join(["---"] * len(header))))
         for f in folder.glob("*.html"):
             print(f"\t{f}")
             html: lxml.html.HtmlElement = lxml.html.parse(f).getroot()
@@ -19,12 +20,14 @@ for folder in docs.glob("*"):
             name = f"[{f.stem}](./{f.name})"
             alias = html.find_class("ContentHeader")[0].getparent().text_content().split("\t")[-1]
             description = ""
+            toolset = ""
             for h in html.findall(".//h3"):
                 if h.text == "Description":
                     pre = h.getnext().getnext()
                     if pre is not None:
                         description = pre.text_content()
-                    break
-            writer.write(f"| {name} | {alias} | {description} |\n")
+                elif h.text == "Toolset":
+                    toolset = h.tail
+            writer.write("| {} |\n".format(" | ".join([toolset, name, alias, description])))
 
         writer.write(f"\n`Last built {datetime.date.today()}`\n")
